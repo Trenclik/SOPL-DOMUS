9d7de47f684ce9f9aafb7cd2a5c5d563a1081228
Marat141 <terest141@gmail.com>
2024-12-06 20:32:19 +0100

    Frontent-7-Marat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Foster APP</h1>
 
-<a href="/Login">Login</a>
-<a href="/Register">Register</a>
-<a href="/2FA">2</a>
-<a href="/NewPass">New Pass</a>
-<a href="/ForgotPass">Zapomenuté</a>
