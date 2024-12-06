8abeccf24e1855f05829a816b1e7e4b8d0631565
Marat141 <terest141@gmail.com>
2024-12-06 13:54:29 +0100

    Frontend-1-Marat

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    resolve: {
-        alias: {
-            $lib: path.resolve('./src/lib'), // Ujistěte se, že cesta odpovídá
-        },
-    },
+	plugins: [sveltekit()]
 });
