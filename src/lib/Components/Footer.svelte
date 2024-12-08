2afb8c2da8801568995f4e9bb664011c04955792
Trenclik <trenclikb1@gmail.com>
2024-12-08 07:47:39 +0100

    databaze

--- src/lib/Components/Footer.svelte
+++ src/lib/Components/Footer.svelte
@@ -6,14 +6,18 @@
     <a href="/Search"><img class="lupa" src="./magnifying-glass.svg" alt=""></a>
     <a href="/ProhlizecTemat"><img class="bublina" src="./speechbubble.svg" alt=""></a>
     <a href="/Chat"><img class="bublina-clovek" src="./buble-person.svg" alt=""></a>
-    {#if data?.user}
-        <a href="/Profile"><img src={data.user.profileImage} alt="User Profile"></a>
+    {#if data.user}
+        <a id="pfp" href="/Profile"><img src={data.user.profileImage} alt=""></a>
     {:else}
-        <a href="/Login">Login</a>
+        <a id="pfp" href="/Profile"><img src="https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg" alt=""></a>
     {/if}
 </footer>
 
 <style lang="scss">
+    #pfp{
+        object-fit: cover;
+        border-radius: 50%;
+    }
     footer {
         width: 100vw;
         position: fixed;
@@ -27,14 +31,15 @@
 
         a{
             width: 40px;
+            height: 40px;
             display: flex;
             justify-content: center;
             align-items: center;
+            overflow: hidden;
+            
 
             img{
-                width: 100%;
-                object-fit: cover;
-                border-radius: 50%;
+                height: 100%;
             }
         }
 
